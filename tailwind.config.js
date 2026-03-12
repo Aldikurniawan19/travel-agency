30e35cb6527da85a7fd9334c5ddf0f1b2bd205fd
aldikurniawan19 <aldikurniawann15@gmail.com>
2026-03-12 23:28:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    green: "#1b3228",
+                    orange: "#f26a2e",
+                    light: "#f8fafc",
+                },
+            },
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
